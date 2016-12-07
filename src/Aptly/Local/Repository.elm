acc25e5dd20b41b4c63c8f72b6621aeb0aab8a44
Patrick Pokatilo <mail@shyxormz.net>
2016-12-08 00:31:09 +0100

    PUT local repository when saving

--- src/Aptly/Local/Repository.elm
+++ src/Aptly/Local/Repository.elm
@@ -1,10 +1,11 @@
-module Aptly.Local.Repository exposing (Repository, decodeJson, list, view, viewForm)
+module Aptly.Local.Repository exposing (EditableTarget, Repository, decodeJson, edit, list, update, view, viewForm)
 
 import Html
 import Html.Attributes
 import Html.Events
 import Http
 import Json.Decode
+import Json.Encode
 
 type alias Repository =
     { name : String
@@ -12,6 +13,15 @@ type alias Repository =
     , defaultDistribution : String
     , defaultComponent : String
     }
+
+type EditableTarget
+    = Name
+    | Comment
+    | DefaultDistribution
+    | DefaultComponent
+
+type Msg
+    = Input EditableTarget String
 
 decodeJson : Json.Decode.Decoder Repository
 decodeJson =
@@ -21,9 +31,48 @@ decodeJson =
         (Json.Decode.field "DefaultDistribution" Json.Decode.string)
         (Json.Decode.field "DefaultComponent" Json.Decode.string)
 
+edit : String -> Repository -> Http.Request Repository
+edit server repository =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = (server ++ "/api/repos/" ++ repository.name)
+        , body = Http.jsonBody (encodeJson repository)
+        , expect = Http.expectJson decodeJson
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+encodeJson : Repository -> Json.Encode.Value
+encodeJson repository =
+    Json.Encode.object
+        [ ("Comment", Json.Encode.string repository.comment)
+        , ("DefaultDistribution", Json.Encode.string repository.defaultDistribution)
+        , ("DefaultComponent", Json.Encode.string repository.defaultComponent)
+        ]
+
+init : String -> (Repository, Cmd Msg)
+init name =
+    (Repository name "" "" "", Cmd.none)
+
 list : String -> Http.Request (List Repository)
 list server =
     Http.get (server ++ "/api/repos") (Json.Decode.list decodeJson)
+
+update : EditableTarget -> Repository -> String -> Repository
+update target repository value =
+    case target of
+        Name ->
+            { repository | name = value }
+
+        Comment ->
+            { repository | comment = value }
+
+        DefaultDistribution ->
+            { repository | defaultDistribution = value }
+
+        DefaultComponent ->
+            { repository | defaultComponent = value }
 
 view : (Repository -> msg) -> Repository -> Html.Html msg
 view editMsg repository =
@@ -49,8 +98,8 @@ view editMsg repository =
             ]
         ]
 
-viewForm : Bool -> Repository -> Html.Html msg
-viewForm isNew repository =
+viewForm : Bool -> msg -> (Repository -> msg) -> Repository -> Html.Html msg
+viewForm isNew cancelMsg saveMsg repository =
     Html.form []
         [ Html.label []
             [ Html.text "Name"
@@ -59,19 +108,19 @@ viewForm isNew repository =
         , Html.br [] []
         , Html.label []
             [ Html.text "Comment"
-            , Html.input [ Html.Attributes.value repository.comment ] []
+            , Html.input [ {- Html.Events.onInput <| inputMsg Comment, -} Html.Attributes.value repository.comment ] []
             ]
         , Html.br [] []
         , Html.label []
             [ Html.text "Default Distribution"
-            , Html.input [ Html.Attributes.value repository.defaultDistribution ] []
+            , Html.input [ {- Html.Events.onInput <| inputMsg DefaultDistribution, -} Html.Attributes.value repository.defaultDistribution ] []
             ]
         , Html.br [] []
         , Html.label []
             [ Html.text "Default Component"
-            , Html.input [ Html.Attributes.value repository.defaultComponent ] []
+            , Html.input [ {- Html.Events.onInput <| inputMsg DefaultComponent, -} Html.Attributes.value repository.defaultComponent ] []
             ]
         , Html.br [] []
-        , Html.button [ {- Html.Events.onClick <| cancelMsg -} ] [ Html.text "Cancel" ]
-        , Html.button [ {- Html.Events.onClick <| saveMsg repository -} ] [ Html.text "Save" ]
+        , Html.button [ Html.Attributes.type_ "button", Html.Events.onClick <| cancelMsg ] [ Html.text "Cancel" ]
+        , Html.button [ Html.Attributes.type_ "button", Html.Events.onClick <| saveMsg repository ] [ Html.text "Save" ]
         ]
--- src/LocalRepositoryPage.elm
+++ src/LocalRepositoryPage.elm
@@ -1,6 +1,7 @@
 module LocalRepositoryPage exposing (..)
 
 import Aptly.Local.Repository
+import Debug
 import Html
 import Http
 
@@ -12,6 +13,10 @@ type alias Model =
 type Msg
     = List (Result Http.Error (List Aptly.Local.Repository.Repository))
     | State State
+    | CancelEditing
+    | FinishEditingResult Aptly.Local.Repository.Repository (Result Http.Error Aptly.Local.Repository.Repository)
+    | FinishEditing Aptly.Local.Repository.Repository Aptly.Local.Repository.Repository
+--    | RepositoryMsg
 
 
 type State
@@ -21,6 +26,13 @@ type State
 init : (Model, Cmd Msg)
 init =
     (Model [] Listing, Cmd.none)
+
+replace : List a -> a -> a -> List a
+replace list old new =
+    List.map (\item ->
+        case item == old of
+            True -> new
+            False -> item) list
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -37,6 +49,22 @@ update msg model =
         State (Editing repository) ->
             ({ model | state = Editing repository }, Cmd.none)
 
+        CancelEditing ->
+            ({ model | state = Listing }, Cmd.none)
+
+        FinishEditingResult _ (Err _) ->
+            (model, Cmd.none)
+
+        FinishEditingResult oldRepository (Ok newRepository) ->
+            let
+                _ = Debug.log "old" oldRepository
+                _ = Debug.log "new" newRepository
+            in
+                ({ model | repositories = replace model.repositories oldRepository newRepository }, Cmd.none)
+
+        FinishEditing oldRepository newRepository ->
+            (model, Http.send (FinishEditingResult oldRepository) (Aptly.Local.Repository.edit "http://127.0.0.1:8080" newRepository))
+
 view : Model -> Html.Html Msg
 view model =
     Html.div []
@@ -48,4 +76,4 @@ view model =
                     (List.intersperse (Html.hr [] []) <| List.map (Aptly.Local.Repository.view (\repository -> State (Editing repository))) model.repositories)
 
                 Editing repository ->
-                    [ Aptly.Local.Repository.viewForm False repository ]
+                    [ Aptly.Local.Repository.viewForm False (CancelEditing) (FinishEditing repository) repository ]
